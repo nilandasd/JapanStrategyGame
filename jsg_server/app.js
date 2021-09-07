c95f0ca6a17fefebca7380c04c6349a1eeeac000
Niland Schumacher <naschuma@ucsc.edu>
2021-09-07 11:07:46 -0700

    added auth to the backend, and connected database

--- jsg_server/app.js
+++ jsg_server/app.js
@@ -1,21 +1,54 @@
+require("dotenv").config();
 const path = require("path");
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const app = express();
-const https = require("https");
-const fs = require("fs");
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const bcrypt = require("bcrypt");
-// const secret = fs.readFileSync("jwt.key");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const gameRoutes = require("./routes/game");
+const server = require("https");
+const PORT = process.env.PORT || 5000;
+const uri = process.env.URI;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
 
 app.use(express.static("build"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["https://localhost:5000"],
+    credentials: true,
+  })
+);
 
-
-
+app.use("/auth", authRoutes);
+app.use("/game", gameRoutes);
 app.use("/", (req, res) => {
-    console.log(req.body);
-    res.status(200).json({message: "success!"});
+  res.sendFile(path.join(__dirname, "./build/index.html"));
 });
+
+//SELF SIGNED!
+server
+  .createServer(
+    {
+      key: process.env.PRIVATE_KEY,
+      cert: process.env.CERTIFICATE,
+    },
+    app
+  )
+  .listen(PORT, () => console.log(`server listening on port: ${PORT}`));
